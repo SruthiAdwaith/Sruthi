96f4056eb1d5fa9476d25ab53bed84d75f4c1214
SruthiAdwaith <sruthikalarikkal12690@gmail.com>
2016-10-17 08:51:23 -0700

    fb service partly oct 17

---
+++ Facebookservicestrutsv2/src/com/user/User.java
@@ -0,0 +1,30 @@
+package com.user;
+
+public class User {
+	
+  public String email;
+  public String password;
+  
+	public void setEmail(String str_username)
+	{
+		email=str_username;	
+	}
+	public String getEmail()
+	{
+		return this.email;
+	}
+	public void setPassword(String str_password)
+	{
+	
+		password=str_password;
+	}
+	public String getPassword()
+	{
+		return this.password;
+	}
+  
+  
+  
+  
+  
+}
